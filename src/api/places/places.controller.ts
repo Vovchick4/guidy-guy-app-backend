7d0fc9699fccdb12692a8ccf040cb385a5025173
Vovchick4 <vovaspodpivas@gmail.com>
2022-06-10 01:07:51 +0300

    add crud operations palces

--- src/api/places/places.controller.ts
+++ src/api/places/places.controller.ts
@@ -1,4 +1,4 @@
-import { Inject, Controller, Get, Post, Param, ParseIntPipe, Body } from '@nestjs/common';
+import { Inject, Controller, Get, Post, Patch, Param, ParseIntPipe, Body, ParseEnumPipe, Delete, Query } from '@nestjs/common';
 
 import { Place } from './places.entity';
 import { PlacesService } from './places.service';
@@ -9,13 +9,28 @@ export class PlacesController {
     @Inject(PlacesService)
     private readonly services: PlacesService;
 
-    @Get(':id')
-    public getPlaceById(@Param('id', ParseIntPipe) id: number): Promise<Place> {
-        return this.services.getPlaceById(id);
+    @Get('/:placeId')
+    public async findPlaceById(@Param('placeId', ParseIntPipe) placeId: number): Promise<Place> {
+        return await this.services.findPlaceById(placeId);
+    }
+
+    @Get()
+    public async findPlace(@Query('params') params: object): Promise<Place[]> {
+        return await this.services.findPlace(params);
     }
 
     @Post()
-    public createPlace(@Body() body: CreatePlaceDto): Promise<Place> {
-        return this.services.createPlace(body);
+    public async createPlace(@Body() body: CreatePlaceDto): Promise<Place> {
+        return await this.services.createPlace(body);
+    }
+
+    @Patch('/:placeId')
+    public async updatePlace(@Param('placeId', ParseIntPipe) placeId: number, @Body() body: CreatePlaceDto): Promise<Place> {
+        return await this.services.updatePlace(placeId, body);
+    }
+
+    @Delete('/:placeId')
+    public async removePlace(@Param('placeId', ParseIntPipe) placeId: number): Promise<void> {
+        return await this.services.removePlace(placeId);
     }
 }
--- src/api/places/places.service.ts
+++ src/api/places/places.service.ts
@@ -10,7 +10,7 @@ export class PlacesService {
     @InjectRepository(Place)
     private readonly repository: Repository<Place>
 
-    public async getPlaceById(id: number): Promise<Place> {
+    public async findPlaceById(id: number): Promise<Place> {
         const findElem = await this.repository.findOne({ where: { id } });
 
         if (!findElem) {
@@ -20,9 +20,29 @@ export class PlacesService {
         return findElem;
     }
 
-    public createPlace(body: CreatePlaceDto): Promise<Place> {
+    public async findPlace(params: object): Promise<Place[]> {
+        const findPlace = await this.repository.find({})
+        if (!findPlace) {
+            throw new HttpException('Not Found Place', HttpStatus.NOT_FOUND)
+        }
+
+        return findPlace;
+    }
+
+    public async createPlace(body: CreatePlaceDto): Promise<Place> {
         const place: Place = new Place();
         place.name = body.name;
-        return this.repository.save(place);
+        return await this.repository.save(place);
+    }
+
+    public async updatePlace(id: number, body: CreatePlaceDto): Promise<Place> {
+        const findPlace: any = this.findPlaceById(id);
+        findPlace.name = body.name;
+        return await this.repository.save(findPlace);
+    }
+
+    public async removePlace(id: number): Promise<void> {
+        const findPlace: any = this.findPlaceById(id);
+        await this.repository.remove(findPlace);
     }
 }
