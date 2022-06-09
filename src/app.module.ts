fa9857f5d3bd8aee66643209574a2da94c9796bc
Vovchick4 <vovaspodpivas@gmail.com>
2022-06-09 19:08:41 +0300

    add envs, common setup typeorm config, connect to postgressql.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// Envs
+import { getEnvPath } from './common/helper/env.helper';
+
+// TypeOrmService
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+
+// Set env Files '
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
